dca869c621e00459515b9c0da385bef585477fae
gavinford <gavinf902@gmail.com>
2017-11-29 11:58:28 -0800

    Added yourinitials

---
+++ yourintials/src/yourintials/yourintials.java
@@ -0,0 +1,12 @@
+package yourintials;
+
+public class yourintials 
+{
+	System.out.println("Gavin Angus Ford");
+	System.out.println(" GGG     A    FFFFF");
+	System.out.println("G   G   A A   F    ");
+	System.out.println("G      A   A  F    ");
+	System.out.println("GGGGG  AAAAA  FFF  ");
+	System.out.println("G   G  A   A  F    ");
+
+}
