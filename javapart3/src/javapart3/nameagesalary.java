100dedbaccdd6bb590047d7b9103358dd8dc4640
gavinford <gavinf902@gmail.com>
2017-12-01 11:12:45 -0800

    working on part3 java

---
+++ javapart3/src/javapart3/nameagesalary.java
@@ -0,0 +1,23 @@
+package javapart3;
+import java.util.Scanner;
+public class nameagesalary 
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		String name;
+		int age;
+		double salaryph;
+		
+		System.out.println("Hello. What's your name?");
+		name=keyboard.next();
+		
+		System.out.println("Hey, "+name+"! how old are you?");
+		age=keyboard.nextInt(); 
+		
+		System.out.println("So you are "+age+"? I guess that's not too old, "+name+". How much do you make per hour?");
+		salaryph=keyboard.nextDouble();
+	}
+	
+}
