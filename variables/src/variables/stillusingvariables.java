897f6059b8a2a6b346732ad828ccab23dfe96d98
gavinford <gavinf902@gmail.com>
2017-12-01 10:13:48 -0800

    finsihed part2 (variables)

---
+++ variables/src/variables/stillusingvariables.java
@@ -0,0 +1,14 @@
+package variables;
+public class stillusingvariables 
+{
+		public static void main(String[] args)
+		{
+			String name;
+			int gradyear;
+			
+			name = "Gavin Ford";
+			gradyear = 2021;
+			
+			System.out.println("My name is "+name+" and I'll graduate in "+gradyear+".");
+		}
+}
